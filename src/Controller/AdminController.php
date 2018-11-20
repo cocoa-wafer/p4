a15a657e7386e5a400c279bf71fb28fc4e77cb96
audreyg <audreyg@users.noreply.github.com>
2018-11-20 13:16:56 +0100

    admin base

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,5 +1,35 @@
 <?php 
 
+require_once '../Model/AdminManager.php';
+require_once '../Model/Admin.php';
+require_once 'Controller.php';
+
 class  AdminController extends Controller {
+    
+  public function addAdmin($login,$password) { 
+    $admin = new Admin([
+        'login' => $login,
+        'password' => $password
+    ]);
+      
+
+    $this->adminManager->addAdmin($admin);
+    // render twig ici render view avant ajout mais avec celui en plus
+    // issue: comment récup les infos précédentes pour les render ? session infos ?
+  }    
+    
+  public function deleteAdmin($id) { 
+
+    $this->adminManager->deleteAdmin($id);
+      
+    // render view twig même que avant click sup mais sans le post en question 
+    // issue : comment recup donnees utilisees dans le twig precedent ? session infos ?
+  }    
+  public function getAdmin($id) { 
+
+    $this->adminManager->getAdmin($id);
+    // render view twig avec tableau constitué des valeurs setters de getPost
+  }    
+
     
 }
--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -1,6 +1,59 @@
 <?php 
 
+require_once 'Admin.php';
+require_once 'Manager.php';
+
 class AdminManager extends Manager {
-        
     
+    public function addAdmin(Admin $admin) {
+        
+        try {
+        
+          $q=$this->_db->prepare('INSERT INTO admin(login,password) VALUES(:login,:password');
+          $q->execute(array(
+          'login'=>$admin->getLogin(),
+          'password'=>$admin->getPassword(),
+      ));
+          echo 'admin ajouté';
+        
+      }
+      catch (Exception $e) {
+          'erreur'.$e->getMessage(); 
+      }
+        
+    }
+    
+    public function deleteAdmin($id) {
+        
+        try{
+          $q = $this->_db->query('DELETE FROM admin WHERE id ='.$id);
+          echo 'admin supprimé';
+      }
+      catch (Exception $e) {
+          echo 'Erreur' . $e->getMessage();
+      }
+        
+    }
+    
+    public function getAdmin($id) {
+        
+        try{
+          $q = $this->_db->query('SELECT * FROM admin WHERE id='.$id);
+          $donnees = $q->fetch();
+        return new Admin($donnees);
+    
+  }    
+      
+        catch (Exception $e) {
+          echo 'Erreur' . $e->getMessage();
+      }
+      
 }
+        
+    }
+
+    
+    
+    
+
+
