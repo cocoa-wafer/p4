d22569c543653000416d0a7bc14edf36e206555f
audreyg <audreyg@users.noreply.github.com>
2018-11-24 16:54:49 +0100

    tests twig

--- index.php
+++ index.php
@@ -8,18 +8,40 @@
 require 'vendor/autoload.php' ;
 
 //transformer les require en nametags
-require 'PostController';
+/* require 'PostController';
 require 'CommentController';
-require 'AdminController';
+require 'AdminController'; */
 
 //test twig.
-$loader = new Twig_Loader_Array(array(
+/* $loader = new Twig_Loader_Array(array(
     'index' => 'Hello {{ name }}!',
 ));
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index', array('name' => 'Fabien'));
-
-
+echo $twig->render('index', array('name' => 'Fabien')); */
 
 // recup donnees qui transitent, les tester pour validité (les échapper ici ?) puis lancer controller adéquat.
+
+
+$page = 'home';
+
+if (isset($_GET['p'])) {
+    $page = $_GET['p'];
+}
+
+
+//rendu twig
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/src/View');
+$twig = new \Twig_Environment($loader, [
+    'cache' => false,
+]);
+
+
+
+switch ($page) {
+    case 'home': echo $twig->render('home.twig');
+        break;
+    case 'contact' : echo $twig->render('contact.twig');
+        break;
+    default : echo 'page introuvable';
+}
